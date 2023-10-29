66bc17159f35025d84f695154198443ded94619e
Chocos coding <chocoscoding@gmail.com>
2023-10-29 16:31:52 +0000

    done

--- src/components/CustomCursor/Index.jsx
+++ src/components/CustomCursor/Index.jsx
@@ -4,20 +4,16 @@ const Index = () => {
   const cursorRef = useRef(null);
 
   useEffect(()=>{
-    const mouseTransformer = (e5)=>{
-      console.log('ds')
-      const {clientX, clientY} = e5;
-      const mouseX = clientX - cursorRef.current.clientWidth/2;
-      const mouseY = clientY - cursorRef.current.clientHeight/2;
-      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`
-    }
-    document.addEventListener('mousemove', (event)=>{
-      console.log('ds')
+    const mouseTransformer = (event)=>{
       const {clientX, clientY} = event;
-      const mouseX = clientX - cursorRef.current.clientWidth)/2;
+      const mouseX = clientX - (cursorRef.current.clientWidth/2);
       const mouseY = clientY - (cursorRef.current.clientHeight/2);
-      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`
-    })
+      cursorRef.current.style.left = `${mouseX}px`
+      cursorRef.current.style.top = `${mouseY}px`
+    }
+    document.addEventListener('mousemove', mouseTransformer)
+
+    return ()=> document.removeEventListener('mousemove', mouseTransformer)
   
   }, [])
   return (
